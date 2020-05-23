407316810e0521ba3181e170faf047d11ce95e5e
Cryton Andrew <crytonadrew21@gmail.com>
2020-05-23 12:30:37 +0200

    Initial Commit

---
+++ Edu-Spec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Edu-Spec
+//
+//  Created by Cryton Sibanda on 2020/05/23.
+//  Copyright © 2020 Cryton Sibanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
